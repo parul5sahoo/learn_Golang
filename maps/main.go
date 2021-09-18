83dc480116d1527933f918be6c911d11e2254720
Parul Sahoo <parulsahoo5jan@gmail.com>
2021-09-18 10:31:49 +0530

    learned about maps

---
+++ maps/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+    // to declare a map
+	//var colors map[string]string
+
+
+  colors := map[string]string{
+ 	  "red": "#FF0000",
+  	  "green": "#4hr98u1",
+ 	  "blue": "#9c0081",
+    }
+
+  //  colors := make(map[string]string)
+
+ //  colors["white"] = "#ffffff"
+
+  //to delete a map
+ // delete(colors, "white")
+
+   //fmt.Println(colors)
+  PrintMap(colors)
+}
+
+func PrintMap(c map[string]string) {
+    for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
